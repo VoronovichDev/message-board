6bc9a086263e58ced8902b961593c499691d30ca
Voronovich_dev <alexanderbizv1@gmail.com>
2022-12-12 20:45:37 +0300

    feat: add post-page, post length validation and mock submit handler

---
+++ pages/post.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { auth, db } from '../utils/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/router';
+
+export default function Post() {
+  const [post, setPost] = useState({ description: '' });
+
+  // Post submission
+
+  const submitPost = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
+      <form onSubmit={submitPost}>
+        <h1 className="text-2xl font-bold">Add a new post</h1>
+        <div className="py-2">
+          <h3 className="text-lg font-medium py-2">Share your thoughts...</h3>
+          <textarea
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
+            value={post.description}
+            className="bg-sky-200 h-48 w-full rounded-lg p-2 text-sm focus:outline-sky-700"
+          ></textarea>
+          <p
+            className={`text-indigo-500 font-medium text-sm ${
+              post.description.length > 280 ? 'text-red-600' : ''
+            } `}
+          >
+            {post.description.length}/280
+          </p>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-indigo-600 opacity-80 text-white rounded-lg p-2 my-2 text-sm hover:opacity-100 transition-opacity duration-300"
+        >
+          Post
+        </button>
+      </form>
+    </div>
+  );
+}
