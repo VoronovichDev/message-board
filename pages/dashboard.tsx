a540627d1bfc925fcb6f9332d85e2477ba4cded8
Voronovich_dev <alexanderbizv1@gmail.com>
2022-12-18 15:48:09 +0300

    feat: render own posts in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,35 @@
-import React, { useEffect } from 'react';
-import { auth } from '../utils/firebase';
+import React, { use, useEffect, useState } from 'react';
+import { auth, db } from '../utils/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/router';
+import { collection, onSnapshot, query, Timestamp, where } from 'firebase/firestore';
+import Message from '../components/message';
+
+interface IDashPostData {
+  description?: string;
+  avatar?: string;
+  timestamp?: Timestamp;
+  user?: string;
+  username?: string;
+  id?: string;
+}
 
 export default function Dashboard() {
   const route = useRouter();
   const [user, loading, error] = useAuthState(auth);
+  const [posts, setPosts] = useState<IDashPostData[]>([]);
+  console.log(user);
 
   useEffect(() => {
     const getData = async () => {
       if (loading) return;
       if (!user) return route.push('./auth/login');
+      const collectionRef = collection(db, 'posts');
+      const q = query(collectionRef, where('user', '==', user.uid));
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      });
+      return unsubscribe;
     };
     getData();
   }, [user, loading, route]);
@@ -18,7 +37,11 @@ export default function Dashboard() {
   return (
     <div>
       <h1>Your posts</h1>
-      <div>posts</div>
+      <div>
+        {posts.map((post) => (
+          <Message {...post} key={post.id}></Message>
+        ))}
+      </div>
       <button onClick={() => auth.signOut()}>Sign Out</button>
     </div>
   );
