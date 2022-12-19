d2abd1f4ac34cc43c279595abbba5a64f7256312
Voronovich_dev <alexanderbizv1@gmail.com>
2022-12-19 19:26:21 +0300

    feat: render all comments, add checks and conditional render

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,15 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect } from 'react';
 import Message from '../components/message';
 import { useRouter } from 'next/router';
 import { auth, db } from '../utils/firebase';
 import { toast } from 'react-toastify';
-import { arrayUnion, doc, Timestamp, updateDoc } from 'firebase/firestore';
+import { arrayUnion, doc, getDoc, onSnapshot, Timestamp, updateDoc } from 'firebase/firestore';
+
+interface IAllMessages {
+  message?: string;
+  avatar?: string;
+  userName?: string;
+  time?: string;
+}
 
 export default function Details() {
   const router = useRouter();
   const routeData = router.query;
   const [message, setMessage] = useState('');
-  const [allMessages, setAllMessages] = useState([]);
+  const [allMessages, setAllMessages] = useState<IAllMessages[]>([]);
 
   // Submit comment
   const submitMessage = async () => {
@@ -38,6 +46,19 @@ export default function Details() {
     setMessage('');
   };
 
+  useEffect(() => {
+    // Get comments
+    if (!router.isReady) return;
+    const getComments = async () => {
+      const docRef = doc(db, 'posts', routeData.id as string);
+      const unsubscribe = onSnapshot(docRef, (snapshot) => {
+        setAllMessages(snapshot.data()?.comments);
+      });
+      return unsubscribe;
+    };
+    getComments();
+  }, [allMessages, routeData.id, router.isReady]);
+
   return (
     <div>
       <Message {...routeData}></Message>
@@ -56,11 +77,19 @@ export default function Details() {
         </div>
         <div className="py-6">
           <h2 className="font-bold">Comments</h2>
-          {allMessages.map((message) => (
-            <div>
-              <div>qq</div>
-            </div>
-          ))}
+          {allMessages ? (
+            allMessages.map((message) => (
+              <div className="bg-white p-4 my-4 border-2" key={message.time}>
+                <div className="flex items-center gap-2 mb-4">
+                  <img className="w-10 rounded-full" src={message.avatar} alt="user avatar" />
+                  <h2>{message.userName}</h2>
+                </div>
+                <h2>{message.message}</h2>
+              </div>
+            ))
+          ) : (
+            <div>no comments yet</div>
+          )}
         </div>
       </div>
     </div>
